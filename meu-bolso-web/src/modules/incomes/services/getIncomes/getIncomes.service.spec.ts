a96689389f69984af2d16d5705e1c0fc1bff4919
Rafael Oliveira <rafasdoliveira@outlook.com>
2026-02-09 20:50:04 -0300

    voltando

--- meu-bolso-web/src/modules/incomes/services/getIncomes/getIncomes.service.spec.ts
+++ meu-bolso-web/src/modules/incomes/services/getIncomes/getIncomes.service.spec.ts
@@ -1,28 +1,49 @@
 import { describe, it, expect, vi, beforeEach } from 'vitest'
 import type { AxiosInstance } from 'axios'
-import { GetIncomesStatusService } from './getIncomesStatus.service'
+import { GetIncomesService } from './getIncomes.service'
 import type { ApiResponse } from '@shared/types/apiResponse'
-import type { GetIncomesStatusOutputDto } from './getIncomesStatus.dto'
+import type {
+  GetIncomesIntputDto,
+  GetIncomesOutputDto
+} from './getIncomes.dto'
 
-describe('GetIncomesStatusService', () => {
+describe('GetIncomesService', () => {
   let apiMock: AxiosInstance
-  let service: GetIncomesStatusService
+  let service: GetIncomesService
 
   beforeEach(() => {
     apiMock = {
       get: vi.fn()
     } as unknown as AxiosInstance
 
-    service = new GetIncomesStatusService(apiMock)
+    service = new GetIncomesService(apiMock)
   })
 
-  it('deve buscar o status de incomes e retornar corretamente o payload', async () => {
-    const incomesStatus: GetIncomesStatusOutputDto = {
-      status: 'ACTIVE'
+  it('deve buscar incomes e retornar corretamente o payload', async () => {
+    const params: GetIncomesIntputDto = {
+      page: 1,
+      size: 10
     }
 
-    const apiResponse: ApiResponse<GetIncomesStatusOutputDto> = {
-      data: incomesStatus,
+    const incomes: GetIncomesOutputDto = {
+      page: 1,
+      size: 10,
+      total: 1,
+      data: [
+        {
+            id: 1,
+            source: 'Salário',
+            amount: '5000',
+            date: '',
+            notes: '',
+            paymentType: '',
+            status: ''
+        }
+      ]
+    }
+
+    const apiResponse: ApiResponse<GetIncomesOutputDto> = {
+      data: incomes,
       message: 'success',
       status: 'success'
     }
@@ -31,18 +52,29 @@ describe('GetIncomesStatusService', () => {
       data: apiResponse
     } as unknown)
 
-    const result = await service.execute()
+    const result = await service.execute(params)
 
     expect(apiMock.get).toHaveBeenCalledOnce()
-    expect(apiMock.get).toHaveBeenCalledWith('/income-status')
-    expect(result).toEqual(incomesStatus)
+    expect(apiMock.get).toHaveBeenCalledWith('/incomes', {
+      params: {
+        page: 1,
+        size: 10
+      }
+    })
+
+    expect(result).toEqual(incomes)
   })
 
   it('deve propagar erro quando a API falhar', async () => {
+    const params: GetIncomesIntputDto = {
+      page: 1,
+      size: 10
+    }
+
     const error = new Error('API error')
 
     vi.mocked(apiMock.get).mockRejectedValueOnce(error)
 
-    await expect(service.execute()).rejects.toThrow('API error')
+    await expect(service.execute(params)).rejects.toThrow('API error')
   })
 })
